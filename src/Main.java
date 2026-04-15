751633c41201ba2aa9ea861dc19014c9d08a6079
Matheus <matheuspana@outlook.com>
2026-04-14 21:47:59 -0300

    Criação da primeira classe + uso de gets e sets para teste simples

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Pessoa user = new Pessoa();
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Qual o seu nome?");
-        String nome = scanner.nextLine();
-        System.out.println("Hello and welcome, "+ nome +"! Vamos aprender a contar? Digite 1 para SIM e 0 para NÃO");
+        user.setNome(scanner.nextLine());
+        System.out.println("Hello and welcome, "+ user.getNome() +"! Vamos aprender a contar? Digite 1 para SIM e 0 para NÃO");
         int resposta = scanner.nextInt();
         if (resposta == 1) {
             System.out.println("Qual sua idade?");
-            int idade = scanner.nextInt();
-            for (int i = 1; i <= idade; i++) {
-                if (i == idade) {
+            user.setIdade(scanner.nextInt());
+            for (int i = 1; i <= user.getIdade(); i++) {
+                if (i == user.getIdade()) {
                     System.out.println(i + "!");
                     return;
                 }
                 System.out.print(i + ", ");
             }
         } else {
-            System.out.println("Ok " + nome + "! Tenha uma ótima noite!");
+            System.out.println("Ok " + scanner.nextLine() + "! Tenha uma ótima noite!");
         }
 
     }
