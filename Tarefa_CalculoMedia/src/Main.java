c13280b1c0261e61f0de93cb6ac5d7aa53bd0f7d
Matheus <matheuspana@outlook.com>
2026-04-16 19:20:04 -0300

    Atividade Modulo 8

---
+++ Tarefa_CalculoMedia/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Notas notas = new Notas();
+
+        System.out.println("Digite o valor da nota 1");
+        notas.setNota1(Float.parseFloat(scanner.nextLine()));
+        System.out.println("Digite o valor da nota 2");
+        notas.setNota2(Float.parseFloat(scanner.nextLine()));
+        System.out.println("Digite o valor da nota 3");
+        notas.setNota3(Float.parseFloat(scanner.nextLine()));
+        System.out.println("Digite o valor da nota 4");
+        notas.setNota4(Float.parseFloat(scanner.nextLine()));
+
+        notas.calculoMedia();
+    }
+}
