3e2cf1e86addc7528354ad1e4786572dd9c0a952
Matheus <matheuspana@outlook.com>
2026-04-20 17:48:41 -0300

    Atividade Modulo 11

---
+++ Colecoes-parte-2/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //List<Tipo da lista> nomedalista = new ArrayList<Tipo da lista>();
+        List<Integer> nomedalista = new ArrayList<Integer>();
+        nomedalista.add(1);
+        nomedalista.add(3);
+        nomedalista.add(4);
+        nomedalista.add(2);
+        System.out.println(nomedalista);
+    }
+}
