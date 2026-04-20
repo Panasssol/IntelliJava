0d4c1fde90c7cd09569eb5603426c5f5f4bf2d02
Matheus <matheuspana@outlook.com>
2026-04-20 17:50:03 -0300

    Atividade Modulo 11

--- Colecoes-parte-2/src/Main.java
+++ Colecoes-parte-2/src/Main.java
@@ -5,11 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         //List<Tipo da lista> nomedalista = new ArrayList<Tipo da lista>();
-        List<Integer> nomedalista = new ArrayList<Integer>();
-        nomedalista.add(1);
-        nomedalista.add(3);
-        nomedalista.add(4);
-        nomedalista.add(2);
-        System.out.println(nomedalista);
+        List<Integer> listaNumeros = new ArrayList<Integer>();
+        listaNumeros.add(1);
+        listaNumeros.add(3);
+        listaNumeros.add(4);
+        listaNumeros.add(2);
+        System.out.println(listaNumeros);
+
+        List<String> listaNomes = new ArrayList<String>();
+        listaNomes.add("Matheus");
+        listaNomes.add("Panassol");
+        listaNomes.add("DEF");
+        listaNomes.add("ABC");
+        System.out.println(listaNomes);
     }
 }
