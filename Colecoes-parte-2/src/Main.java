800f5145a351ce6d12bf084b080172209f186d6e
Matheus <matheuspana@outlook.com>
2026-04-20 22:46:04 -0300

    Atividade Modulo 11

--- Colecoes-parte-2/src/Main.java
+++ Colecoes-parte-2/src/Main.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
 
         //List<Tipo da lista> nomedalista = new ArrayList<Tipo da lista>();
-        List<Integer> listaNumeros = new ArrayList<Integer>();
-        listaNumeros.add(1);
-        listaNumeros.add(3);
-        listaNumeros.add(4);
-        listaNumeros.add(2);
+        List<Double> listaNumeros = new ArrayList<Double>();
+        listaNumeros.add(1.0);
+        listaNumeros.add(3.0);
+        listaNumeros.add(11.0);
+        listaNumeros.add(4.0);
+        listaNumeros.add(2.1);
+        listaNumeros.add(2.0);
+        System.out.println("Lista de numeros não ordenada");
+        System.out.println(listaNumeros);
+
+        System.out.println("Lista de numeros ordenada");
+        Collections.sort(listaNumeros);
         System.out.println(listaNumeros);
 
         List<String> listaNomes = new ArrayList<String>();
@@ -17,6 +25,11 @@ public class Main {
         listaNomes.add("Panassol");
         listaNomes.add("DEF");
         listaNomes.add("ABC");
+        System.out.println("Lista de nomes não ordenada");
+        System.out.println(listaNomes);
+
+        System.out.println("Lista de nomes ordenada");
+        Collections.sort(listaNomes);
         System.out.println(listaNomes);
     }
 }
